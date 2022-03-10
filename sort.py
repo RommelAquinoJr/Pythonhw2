cc102d1c86366c9e2603a1cedec34b47e00b21c1
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-10 10:49:37 -0800

    updated sort_list function

--- sort.py
+++ sort.py
@@ -1,2 +1,10 @@
-def sort_list():
-
+def sort_list(myList):
+  n = len(myList)
+  i = 0
+  while i < n
+    while n-i-1 >= 0
+      if i > i+1
+        temp = i+1
+        i+1 = i
+        i = temp
+    i += 1

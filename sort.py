4fbfc0331287bb6753f5c3ab750960f77bb7d6bf
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-10 20:53:48 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,14 +2,14 @@ def sort_list(myList):
     list = []
     list.extend(myList)
     n = len(list)
-    i = n - 1
-    j = i - 1
-    while i > 0:
-        while j >= 0:
-            if list[i] < list[j]:
+    i = 0
+    j = 0
+    while i < n:
+        while j < n:
+            if list[i] > list[j]:
                 temp = list[j]
                 list[j] = list[i]
                 list[i] = temp
-            j = j - 1
-        i = i - 1
+            j+=1    
+        i+=1        
     return list

20628a4d162183c01194aa29c806102780b63a51
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-10 19:11:46 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-def sort_list(myList):
+def sort_list(myList):'''
     list = []
     list.extend(myList)
     i = 0
@@ -12,5 +12,8 @@ def sort_list(myList):
         #    n+=1
         i = i+1
         n = n+1
+    return list'''
+    list = []
+    list.extend(myList)
+    list.sort(key = int)
     return list
-    

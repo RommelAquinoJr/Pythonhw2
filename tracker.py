fce1c75d385e9c231db65442edc42f1d63b30877
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-10 23:49:29 -0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,7 +1,10 @@
 def func_counter(func):
 
-  def counter():
+  def counter(x):
     counter.count +=1
-    
+    return func(x)
   counter.count = 0
-  return func
+  return counter
+
+def function(x):
+  return x
